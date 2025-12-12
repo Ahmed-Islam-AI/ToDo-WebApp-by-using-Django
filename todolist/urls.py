15acb144d4cd4fd01d7ce895649bea9fca0b4729
Ahmed-Islam-AI <thecs1122@gmail.com>
2025-12-12 14:37:32 +0500

    base todolist and urls

---
+++ todolist/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path('todolist/', views.todolist, name='todolist'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    
+]
